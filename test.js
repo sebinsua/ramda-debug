5112c84db202cf89fd7d7fb515c2d6974bd19b8b
Seb Insua <me@sebinsua.com>
2015-05-04 22:17:01 +0100

    New TODOs.

--- test.js
+++ test.js
@@ -16,10 +16,9 @@ var entities = [{
 }];
 mapNames(entities);
 
-// TODO: Should not print out multiple executions.
 // TODO: R.look() must be able to take default name as first argument.
-// TODO: See: https://github.com/raine/treis
+// TODO: Separate logging from execution through use of emit/flush.
+// TODO: Should not print out multiple executions.
 
 // TODO: Make the execution signature more concise when large amounts of data are handled.
-// TODO: Separate logging from execution through use of emit/flush.
 // TODO: Make it easy to choose which functions are logged and in what order.
